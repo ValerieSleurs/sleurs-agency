fc1cb0a1048ad83591be61eeabf6fd35d4f38fb4
ValerieSleurs <sleurs.valerie@gmail.com>
2022-11-24 12:56:50 +0100

    Gatbsy Plugins finished

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 import * as React from 'react'
 import { Link } from 'gatsby';
 import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const IndexPage = () => {
   return (
     <main>
       <Layout pageTitle={"Welcome to Sleurs Agency"}>
       <p>Lorem ipsum</p>
+      <StaticImage 
+        alt="randomized unsplash image"
+        src="../images/example.png"
+      />
       </Layout> 
     </main>
   )
